6ed589327afda0b256d274450ce4b41eab999a32
islam <islamdariyah4@gmail.com>
2026-07-29 11:39:44 +0300

    commit

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { BooksService } from './books.service';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
 
-  @Get('/:value')
-  searchBooks(@Param('value') value: string) {
-    return this.booksService.searchBooks(value);
+  // @Get('/:value')
+  // searchBooks(@Param('value') value: string) {
+  //   return this.booksService.searchBooks(value);
+  // }
+
+  // @Get("/: filteredBooksByName")
+  // filteredBooksByName( @Query('price') price:string ,@Query('title') title:string ){
+  //    return this.booksService.filteredBooks(Number(price), title);
+  // }
+
+   
+  @Get('minMaxPrice')
+  minMaxPrice(@Query('minPrice') minPrice: string, @Query('maxPrice') maxPrice: string) {
+    return this.booksService.filteredByPrice(Number(maxPrice), Number(minPrice));
+  }
+
+  @Post('changePrice')
+  changePrice(@Body('id') id: string, @Body('newPrice') newPrice: string) {
+    return this.booksService.changePrice(id, Number(newPrice));
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -2,23 +2,50 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class BooksService {
+  private books = [
+    { id: 1, title: 'Book 1', author: 'Author 1', price: 10 },
+    { id: 4, title: 'Book 1', author: 'Author 1', price: 20 },
+    { id: 2, title: 'Book 2', author: 'Author 2', price: 25 },
+    { id: 3, title: 'Book 3', author: 'Author 3', price: 30 },
+  ];
 
-    private books=[
-        { id: 1, title: 'Book 1', author: 'Author 1' ,price:20},
-        { id: 2, title: 'Book 2', author: 'Author 2' ,price:25},]
+  filteredBooks(price: Number, title: String) {
+    const filtering = this.books.filter(
+      // we used filter method not find method because we want to return all books that match the criteria , find method return just the first match
+      (books) => { return books.title === title && books.price === price;}
+    );
+
+    if (filtering.length > 0) return filtering;
+    else return 'no book found';
+  }
+
+  searchBooks(value: String) {
+    const findValue = this.books.find((book) => {
+      return book.title === value || book.id === Number(value);
+    });
+
+    if (findValue) {
+      return findValue;
+    } else {
+      return 'no book found';
+    }
+  }
 
 
-        
-searchBooks(value: String) {
-    const findValue = this.books.find(
-        (book) => { return book.title === value  || book.id === Number(value) }
-    ); 
+filteredByPrice(priceMax: number, priceMin: number) {
+  const pricee = this.books.filter(
+    (book) => book.price >= priceMin && book.price <= priceMax
+  );
 
-if (findValue) {
-  return findValue;
- }
-  else {
+  if (pricee.length > 0) {
+    return pricee;
+  } else {
     return 'no book found';
   }
+}
+
+changePrice(id: string, newPrice: number) {
+  this.books[Number(id) -1].price = newPrice;
+  return this.books[Number(id)-1];  
 }
 }
